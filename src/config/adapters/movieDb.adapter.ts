2b024eba40c334db56330fecd57c0a4750a0bb75
Allan Montilla <allangmr7@gmail.com>
2024-09-14 00:00:47 -0500

    Add: Remove unnecesary code.

--- src/config/adapters/movieDb.adapter.ts
+++ src/config/adapters/movieDb.adapter.ts
@@ -3,6 +3,6 @@ import { AxiosAdapter } from "./http/axios.adapter";
 export const movieDBFetcher = new AxiosAdapter({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: '803dd525a8500c54d3218f6558ea3ce9',
+        api_key: process.env.THE_MOVIE_DB_KEY || '',
     },
 });
