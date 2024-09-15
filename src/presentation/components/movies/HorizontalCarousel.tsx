8d8fced9ca963b21fdacc497b9a44abc89aecce4
Allan Montilla <allangmr7@gmail.com>
2024-09-14 23:34:26 -0500

    Add: Movies Sections, Render The Movies Images and Create the Scroll Views
    Movies Sections, Render The Movies Images and Create the Scroll Views

---
+++ src/presentation/components/movies/HorizontalCarousel.tsx
@@ -0,0 +1,31 @@
+import { FlatList, Text, View } from "react-native";
+import { Movie } from "../../../core/entities/movie.entity";
+import { MoviePoster } from "./MoviePoster";
+
+interface Props {
+    movies: Movie[];
+    title?: string;
+}
+
+export const HorizontalCarousel = ({movies, title}: Props) => {
+  return (
+    <View
+        style={{ height: title ? 260 : 220, marginTop: 20 }}
+    >
+        {
+            title && (
+                <Text style={{fontSize: 30, fontWeight: 300, marginLeft: 10, marginBottom: 10}}>{title}</Text>
+            )
+        }
+        <FlatList
+            data={movies}
+            renderItem={({item}) => (
+                <MoviePoster movie={item} height={200} width={140} />
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+        />
+    </View>
+  );
+};
--- src/presentation/screen/home/HomeScreen.tsx
+++ src/presentation/screen/home/HomeScreen.tsx
@@ -1,11 +1,28 @@
 import { Text, View } from "react-native";
 import { useMovies } from "../../hooks/useMovies";
+import { ScrollView } from "react-native-gesture-handler";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { PosterCarousel } from "../../components/movies/PosterCarousel";
+import { HorizontalCarousel } from "../../components/movies/HorizontalCarousel";
 
 export const HomeScreen = () => {
-  const {} = useMovies();
+  const {top} = useSafeAreaInsets();
+  const { isLoading, nowPlaying, popular, topRated, upcoming} = useMovies();
+  if(isLoading) {
+    return (<Text>Loading...</Text>);
+  }
   return (
-    <View>
-        <Text>HomeScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={{marginTop: top + 20, paddingBottom: 30}}>
+        {/* Principal Carousel */}
+        <PosterCarousel movies={nowPlaying} />
+        {/* Popular Movies */}
+        <HorizontalCarousel movies={popular} title="Trending" />
+        {/* Top Rated Movies */}
+        <HorizontalCarousel movies={topRated} title="Top Rated" />
+        {/* Popular Movies */}
+        <HorizontalCarousel movies={upcoming} title="Upcominng" />
+      </View>
+    </ScrollView>
   );
 };
